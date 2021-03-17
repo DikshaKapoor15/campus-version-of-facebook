8e896092aae4d8782d23cfbc73d8f485751d9a1a
Ruchika <ruchikasharma0205@gmail.comt>
2021-03-17 11:33:23 +0530

    minor

--- app/application.py
+++ app/application.py
@@ -27,6 +27,8 @@ def load_user(id):
 
 
 @app.route('/', methods=['GET', 'POST'])
+def temp():
+    return render_template("index.html")
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
