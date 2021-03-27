2d4bc8c81e76a665ff0b88b7d973f38deafe4675
abhishek239 <abhishekvusirikala01052001@gmail.com>
2021-03-27 12:00:34 +0530

    Login+register+forgotpassword+createpost

--- app/application.py
+++ app/application.py
@@ -4,6 +4,9 @@ from app.models import *
 from flask import render_template, url_for, redirect,request, jsonify
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 from sqlalchemy import create_engine
+from flask_mail import Mail, Message  ##to be checked
+from app.email import *
+from itsdangerous import URLSafeTimedSerializer ##to be checked
 
 engine = create_engine('postgres://odebgxxluzxqto:02911cc1fe5c97f0916d6a05760b41704668ab6013b712674a3b677f127ac1db@ec2-54-205-183-19.compute-1.amazonaws.com:5432/db0511lmef59sk')
 connection = engine.raw_connection()
@@ -13,12 +16,24 @@ app.config[
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+ts = URLSafeTimedSerializer(app.config["SECRET_KEY"])
 
 # session = db.session()
 # cursor = session.execute(sql).cursor
 
 login = LoginManager(app)
 login.init_app(app)
+
+##mail section to be checked
+mail= Mail(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'developmentsoftware305@gmail.com'
+app.config['MAIL_PASSWORD'] = 'tempmail1@'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
 
 
 @login.user_loader
@@ -27,7 +42,6 @@ def load_user(id):
 
 
 @app.route('/', methods=['GET', 'POST'])
-def wel()
     
 @app.route('/login', methods = ['GET','POST'])
 def login():
@@ -40,7 +54,7 @@ def login():
                 user_object = Credentials.query.filter_by(mail_id = login_form.mail_id.data).first()
                 login_user(user_object)
 
-                return "logged in"
+                return redirect(url_for('create_post'))
         elif request.form["action"] == "Register":
             if reg_form.validate_on_submit():
                 mail_id = reg_form.mail_id.data
@@ -54,5 +68,54 @@ def login():
                 return redirect(url_for('login'))
     return render_template("login.html", form1 = login_form, form = reg_form)
 
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('login'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = Credentials.query.filter_by(mail_id=form.email.data).first()
+        if user:
+            print("user found")
+            send_password_reset_email(user)
+        #flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    idRec = Credentials.verify_reset_password_token(token)
+    print("heyyyyyyappplication""", idRec)
+    # if not user:
+    #     return redirect(url_for('login'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user = Credentials.query.filter_by(mail_id=idRec).first()
+        print("heyy after reset password", user.id)
+        print("heyy before reset password", user.password)
+        user.password=form.password.data
+
+        mycursor.execute("UPDATE credentials SET password = '{password}' WHERE mail_id = '{mail}' ".format(password = str(user.password), mail =  str(idRec)))
+        connection.commit()
+        # user1 = Credentials.query.filter_by(mail_id=idRec).first()
+        print("heyy after reset password in database", user.password)
+        #flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
 
 
+@app.route('/create_post', methods = ['GET','POST'])
+def create_post():
+    post_form = PostForm()
+    if post_form.validate_on_submit():
+        newpost = Posts(mail_id = current_user.mail_id, date = post_form.post_date.data, post_description = post_form.post_description.data, tag1 = post_form.tag1.data, tag2 = post_form.tag2.data, tag3 = post_form.tag3.data )
+        db.session.add(newpost)
+        db.session.commit()
+        return "submited successfully"
+    return render_template('create_post.html',form = post_form)
+
+@app.route("/logout", methods=['GET'])
+def logout():
+    logout_user()
+    return "you are logged out"
--- app/models.py
+++ app/models.py
@@ -1,18 +1,46 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from time import time
+import json
+import jwt
+from app import app
 
 db = SQLAlchemy()
 
 class Credentials(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    mail_id = db.Column(db.String(25), unique = True, nullable = False)
+    mail_id = db.Column(db.String(50), unique = True, nullable = False)
     password = db.Column(db.String(), nullable= False)
+
+    def get_reset_password_token(self, expires_in=6000):
+        return jwt.encode(
+            {'reset_password': self.mail_id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        print("heyymodels",id)
+        return id
 
 
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    mail_id = db.Column(db.String(25), unique = True, nullable = False)
+    mail_id = db.Column(db.String(50), unique = True, nullable = False)
     full_name = db.Column(db.String(), nullable= False)
     year = db.Column(db.Integer, nullable = False)
     department = db.Column(db.String(), nullable= False)
     degree = db.Column(db.String(), nullable= False)
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    mail_id = db.Column(db.String(25), nullable = False)
+    date = db.Column(db.DateTime(timezone=False), nullable=False)
+    post_description =db.Column(db.String(), nullable = False)
+    tag1 = db.Column(db.String(), nullable = False)
+    tag2 = db.Column(db.String())
+    tag3 = db.Column(db.String())
