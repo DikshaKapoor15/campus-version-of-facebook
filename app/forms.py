4b127f06a74597211d652fc78a620835300a9330
sweety879 <58600978+sweety879@users.noreply.github.com>
2021-04-13 18:51:37 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, DateField
-from wtforms.validators import InputRequired, ValidationError,DataRequired, EqualTo
+from wtforms.validators import InputRequired, ValidationError,DataRequired, EqualTo ,Email
 from app.models import *
 import datetime
 import re
@@ -35,13 +35,13 @@ def invalid_password(form,field):
 
 # input fields in login form
 class LoginForm(FlaskForm):
-    mail_id = StringField('mail_id', validators=[InputRequired(message = "Username required"), Email("This field requires a valid email address")])
+    mail_id = StringField('mail_id', validators=[InputRequired(message = "Username required"), Email("This field requires a valid email address")])  #only email-id are accepted
     password = PasswordField('Password', validators=[InputRequired(message = "Password required"),invalid_credentials])
 
 
 # input fields in registration form
 class RegistrationForm(FlaskForm):
-    mail_id = StringField('mail_id',validators=[InputRequired(message = "Mail already exists"), invalid_mail,, Email("This field requires a valid email address")])
+    mail_id = StringField('mail_id',validators=[InputRequired(message = "Mail already exists"), invalid_mail,, Email("This field requires a valid email address")]) #only email-id are accepted
     password = PasswordField('password',validators=[InputRequired(message = "Password mismatch"),invalid_password])
     confirm_password = PasswordField('confirm_password')
     full_name = StringField('full_name')
@@ -53,16 +53,16 @@ class RegistrationForm(FlaskForm):
 # input fields in post form
 class PostForm(FlaskForm):
     mail_id = StringField('mail_id')
-    post_date = DateField(format='%Y-%m-%d')
-    post_description = StringField('post_description')
-    tag1 = StringField('tag1')
-    tag2 = StringField('tag2')
-    tag3 = StringField('tag3')
+    post_date = DateField(format='%Y-%m-%d') # date format YYYY-MM-DD
+    post_description = StringField('post_description')  # post description or caption
+    tag1 = StringField('tag1',validators=[DataRequired()]) # tag related to the event (must tag)
+    tag2 = StringField('tag2') # tag related to the event (optional)
+    tag3 = StringField('tag3') # tag related to the event(optional)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if not self.post_date.data:
-            self.post_date.data = datetime.date.today()
+            self.post_date.data = datetime.date.today() # current date is taken as posted date
 
 
 # input fields in reset password request form
@@ -75,7 +75,7 @@ class ResetPasswordRequestForm(FlaskForm):
 class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')]) #confirming the password
     submit = SubmitField('Request Password Reset')
 
 
