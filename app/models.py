5c3776ead6875d62716527bf5e6804252b1cd341
sweety879 <58600978+sweety879@users.noreply.github.com>
2021-04-22 17:35:52 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -97,16 +97,17 @@ class newprofile(db.Model):
     year = db.Column(db.Integer, nullable = False)
     department = db.Column(db.String(), nullable= False)
     degree = db.Column(db.String(), nullable= False)
-
+    
+# post datatable to store post details of with images
 class Postss(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    mail_id = db.Column(db.String(25), unique=True, nullable=False)
-    date = db.Column(db.DateTime(timezone=False), nullable=False)
-    post_description = db.Column(db.String(),nullable=False)
-    tag1  = db.Column(db.String(),nullable=False)
-    tag2 = db.Column(db.String(),nullable=True)
-    tag3 = db.Column(db.String(),nullable=True)
-    post_img = db.Column(db.LargeBinary, nullable=False)
+    mail_id = db.Column(db.String(25), unique=True, nullable=False)#mail_id of the person who created the post
+    date = db.Column(db.DateTime(timezone=False), nullable=False)# date of the post creation
+    post_description = db.Column(db.String(),nullable=False) # description of the post
+    tag1  = db.Column(db.String(),nullable=False)  # tag (to show is related to a particular event)
+    tag2 = db.Column(db.String(),nullable=True) # tag (to show is related to a particular event)
+    tag3 = db.Column(db.String(),nullable=True) # tag (to show is related to a particular event)
+    post_img = db.Column(db.LargeBinary, nullable=False) # images if anyone person wants to post with image
     reports = db.relationship('PostReport',backref='post',lazy = 'dynamic')
     def __init__(self,mid,d,pd,t1,t2,t3,pi):
         self.mail_id=mid
@@ -118,15 +119,16 @@ class Postss(db.Model):
         self.post_img = pi
         #print(pd,t1,t2,t3,sep=" ")
 
+# post datatable to store post details of without images         
 class post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    mail_id = db.Column(db.String(25), unique=True, nullable=False)
-    date = db.Column(db.DateTime(timezone=False), nullable=False)
-    post_description = db.Column(db.String(), nullable=False)
-    tag1 = db.Column(db.String(), nullable=False)
-    tag2 = db.Column(db.String(), nullable=True)
-    tag3 = db.Column(db.String(), nullable=True)
-    post_img = db.Column(db.LargeBinary, nullable=True)
+    mail_id = db.Column(db.String(25), unique=True, nullable=False) #mail_id of the person who created the post
+    date = db.Column(db.DateTime(timezone=False), nullable=False) # date of the post creation
+    post_description = db.Column(db.String(), nullable=False) # description of the post
+    tag1 = db.Column(db.String(), nullable=False) # tag (to show is related to a particular event)
+    tag2 = db.Column(db.String(), nullable=True)  # tag (to show is related to a particular event)
+    tag3 = db.Column(db.String(), nullable=True)  # tag (to show is related to a particular event)
+    post_img = db.Column(db.LargeBinary, nullable=True) # images if anyone person wants to post with image 
 
     def __init__(self, mid, d, pd, t1, t2, t3, pi):
         self.mail_id = mid
@@ -137,19 +139,19 @@ class post(db.Model):
         self.tag3 = t3
         self.post_img = pi
 
-
+# datatable to store event details
 class events(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(), nullable=False)
-    title = db.Column(db.String(), nullable=False)
-    description = db.Column(db.String(), nullable=False)
-    venue = db.Column(db.String(), nullable=False)
-    tag = db.Column(db.String(), nullable=False)
-    sdate = db.Column(db.DateTime(timezone=False),nullable=False)
-    stime = db.Column(db.DateTime(),nullable=False)
-    edate = db.Column(db.DateTime(timezone=False))
-    etime = db.Column(db.DateTime())
-    color = db.Column(db.String(), nullable=False)
+    username = db.Column(db.String(), nullable=False) # username/mail_id of the person created the event
+    title = db.Column(db.String(), nullable=False) #title of event
+    description = db.Column(db.String(), nullable=False) # descriptoin of event
+    venue = db.Column(db.String(), nullable=False) # venue of the event where it takes place
+    tag = db.Column(db.String(), nullable=False) # tag of the event 
+    sdate = db.Column(db.DateTime(timezone=False),nullable=False) # start date of the event
+    stime = db.Column(db.DateTime(),nullable=False)# start time of the event
+    edate = db.Column(db.DateTime(timezone=False))# end date of the event
+    etime = db.Column(db.DateTime())# end time of the event
+    color = db.Column(db.String(), nullable=False)# color of event to display in the calendar
 
     def __init__(self,un,t,d,v,tag,sd,st,ed,et,c):
         self.username=un
@@ -163,10 +165,11 @@ class events(db.Model):
         self.etime=et
         self.color=c
 
+# datatable to store tags of every event        
 class eventags(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tag = db.Column(db.String(), nullable=False)
-    count = db.Column(db.Integer,nullable=False)
+    tag = db.Column(db.String(), nullable=False) # tag of event
+    count = db.Column(db.Integer,nullable=False) # tag counter shows number of include the coresponding tag
 
     def __init__(self,t,c):
         self.tag= t
