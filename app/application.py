2c4471de55894db6c21b71f49cf37cf7cd2ed17a
sweety <seelasweety38@gmail.com>
2021-04-22 13:33:09 +0530

    master branch

--- app/application.py
+++ app/application.py
@@ -7,6 +7,9 @@ from sqlalchemy import create_engine
 from flask_mail import Mail, Message  ##to be checked
 from app.email import *
 from itsdangerous import URLSafeTimedSerializer ##to be checked
+from werkzeug.security import generate_password_hash,check_password_hash
+import base64
+
 
 engine = create_engine('postgres://odebgxxluzxqto:02911cc1fe5c97f0916d6a05760b41704668ab6013b712674a3b677f127ac1db@ec2-54-205-183-19.compute-1.amazonaws.com:5432/db0511lmef59sk')
 connection = engine.raw_connection()
@@ -54,13 +57,13 @@ def login():
                 user_object = Credentials.query.filter_by(mail_id = login_form.mail_id.data).first()
                 login_user(user_object)
 
-                return redirect(url_for('create_post'))
+                return redirect(url_for('home'))
         elif request.form["action"] == "Register":
             if reg_form.validate_on_submit():
                 mail_id = reg_form.mail_id.data
                 password = reg_form.password.data
-                cred = Credentials(mail_id=mail_id, password=password)
-                prof = Profile(mail_id=mail_id, full_name=reg_form.full_name.data, year=reg_form.year.data,
+                cred = Credentials(mail_id=mail_id, password=generate_password_hash(password))
+                prof = newprofile(mail_id=mail_id, full_name=reg_form.full_name.data, year=reg_form.year.data,
                                department=reg_form.department.data, degree=reg_form.degree.data)
                 db.session.add(cred)
                 db.session.add(prof)
@@ -94,7 +97,7 @@ def reset_password(token):
         user = Credentials.query.filter_by(mail_id=idRec).first()
         print("heyy after reset password", user.id)
         print("heyy before reset password", user.password)
-        user.password=form.password.data
+        user.password=generate_password_hash(form.password.data)
 
         mycursor.execute("UPDATE credentials SET password = '{password}' WHERE mail_id = '{mail}' ".format(password = str(user.password), mail =  str(idRec)))
         connection.commit()
@@ -104,17 +107,201 @@ def reset_password(token):
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
 
+
+
+@app.route('/fetchdata',methods=["POST","GET"])
+def fetchdata():
+    if request.method=="POST":
+        id=request.form['id']
+        mycursor.execute("select * from events where events.tag = '{0}'".format(str(id)))
+        events = mycursor.fetchall()
+        print(id)
+    return jsonify({'htmlresponse':render_template('response1.html',event = events)})
+
+@app.route('/home',methods=["POST","GET"])
+def home():
+    mycursor.execute("select tag from eventags order by count desc")
+    trending = mycursor.fetchall()
+    trending = [x[0] for x in trending]
+    trending = trending[:4]
+    print(trending)
+    hform = HomeForm()
+    mycursor.execute("select id,tag from eventags")
+    value = mycursor.fetchall()
+    value = [(x[0], x[1]) for x in value]
+    value = sorted(value)
+    hform.tag_search.choices = value
+    return render_template('home.html', form=hform,trending=trending)
+
+
+
+@app.route('/homeSearch',methods=["POST","GET"])
+def homeSearch():
+    if request.method == "GET":
+        print("1111")
+        mycursor.execute(
+            "select distinct  p.full_name, po.date,po.post_description,po.tag1,po.post_img,po.tag2,po.tag3,po.id from post as po , newprofile as p where p.mail_id=po.mail_id order by date")
+        data1 = mycursor.fetchall()
+        print("2222")
+        mycursor.execute(
+            "select distinct  p.full_name, po.date,po.post_description,po.tag1,po.post_img,po.tag2,po.tag3,po.id from postss as po , newprofile as p where p.mail_id=po.mail_id order by date")
+        data = mycursor.fetchall()
+        data = [list(x) for x in data]
+        imgs = [base64.b64encode(x[4]) for x in data]
+        imgs = [x.decode('utf-8') for x in imgs]
+
+        for i in range(len(data)):
+            data[i][4] = imgs[i]
+        data.extend(data1)
+        data.sort(key=lambda x: x[1], reverse=True)
+        print(data1)
+        print(list([x[1] for x in data]))
+        return jsonify({"htmlresponse": render_template('response.html', data=data)})
+
+    elif request.method == "POST":
+        mycursor.execute("select id,tag from eventags")
+        tvalue = mycursor.fetchall()
+        tvalue = [(x[0], x[1]) for x in tvalue]
+        tvalue = dict(tvalue)
+        tvalue[0] = ''
+        x = request.form['tag']
+        print(x)
+        x = tvalue.get(int(x))
+        print(x)
+        if x:
+            mycursor.execute( " select distinct  p.full_name, po.date,po.post_description,po.tag1,po.post_img,po.tag2,po.tag3,po.id from postss as po , newprofile as p"
+                " where (p.mail_id=po.mail_id) and (po.tag1='{0}' or po.tag2='{0}' or po.tag3='{0}')  order by date".format( str(x)))
+            data = mycursor.fetchall()
+            mycursor.execute(
+                " select distinct  p.full_name, po.date,po.post_description,po.tag1,po.post_img,po.tag2,po.tag3,po.id from post as po , newprofile as p"
+                " where (p.mail_id=po.mail_id) and (po.tag1='{0}' or po.tag2='{0}' or po.tag3='{0}')  order by date".format(
+                    str(x)))
+            data1 = mycursor.fetchall()
+            data = [list(x) for x in data]
+            imgs = [base64.b64encode(x[4]) for x in data]
+            imgs = [x.decode('utf-8') for x in imgs]
+            for i in range(len(data)):
+                data[i][4] = imgs[i]
+            data.extend(data1)
+            data.sort(key=lambda x: x[1], reverse=True)
+            return jsonify({"htmlresponse": render_template('response.html', data=data)})
+
+        else:
+            return jsonify({"error": "Select a tag "})
+
+    return jsonify({"error": "error"})
+
 @app.route('/create_post', methods = ['GET','POST'])
 def create_post():
-    post_form = PostForm()
-    if post_form.validate_on_submit():
-        newpost = Posts(mail_id = current_user.mail_id, date = post_form.post_date.data, post_description = post_form.post_description.data, tag1 = post_form.tag1.data, tag2 = post_form.tag2.data, tag3 = post_form.tag3.data )
-        db.session.add(newpost)
+    ptform = PosttForm()
+    mycursor.execute("select id,tag from eventags")
+    value = mycursor.fetchall()
+    value = [(x[0], x[1]) for x in value]
+    value = sorted(value)
+    ptform.tag1.choices = value
+    ptform.tag2.choices = value
+    ptform.tag3.choices = value
+    value = dict(value)
+    value[0] = ''
+    if request.method == "POST":
+        if ptform.validate_on_submit():
+            f = request.files['post_img']
+            mycursor.execute("update eventags set count=count+1 where id='{0}'".format(ptform.tag1.data))
+            connection.commit()
+            if ptform.tag2.data != 0:
+                mycursor.execute("update eventags set count=count+1 where id='{0}'".format(ptform.tag2.data))
+                connection.commit()
+            if ptform.tag3.data != 0:
+                mycursor.execute("update eventags set count=count+1 where id='{0}'".format(ptform.tag3.data))
+                connection.commit()
+            if f:
+                newPost = Postss(d=ptform.date.data, pd=ptform.post_description.data,
+                                 t1=value[ptform.tag1.data], t2=value[ptform.tag2.data], t3=value[ptform.tag3.data],
+                                 mid=current_user.mail_id
+                                 , pi=f.read())
+            else:
+                newPost = post(d=ptform.date.data, pd=ptform.post_description.data,
+                               t1=value[ptform.tag1.data], t2=value[ptform.tag2.data], t3=value[ptform.tag3.data],
+                               mid=current_user.mail_id
+                               , pi=f.read())
+
+            db.session.add(newPost)
+            db.session.commit()
+            return "uploaded successfully"
+    return render_template('create_post.html', form=ptform)
+
+@app.route('/like/<int:post_id>/<action>')
+@login_required
+def like_action(post_id, action):
+    print(post_id)
+    if action == 'like':
+        current_user.like_post(post_id)
+
+    if action == 'unlike':
+        current_user.unlike_post(post_id)
+
+    return redirect(request.referrer)
+
+@app.route('/report/<int:post_id>/<action>')
+@login_required
+def report_action(post_id, action):
+    print(post_id)
+    if action == 'report':
+        current_user.report_post(post_id)
+        mycursor.execute("select id, user_id from post_report where post_id = {0} ".format(post_id))
+        reports = mycursor.fetchall()
+        reports = [x[0] for x in reports]
+        no_of_reports = len(reports)
+        print(no_of_reports)
+        if no_of_reports>1 :
+            mycursor.execute("delete from postss where id = {0}".format(post_id))
+            connection.commit()
+    if action == 'unreport':
+        current_user.unreport_post(post_id)
+
+    return redirect(request.referrer)
+
+
+
+@app.route('/calendar', methods=["GET", "POST"])
+def calendar():
+    mycursor.execute("select * from events order by id")
+    data = mycursor.fetchall()
+    timedate1 = [datetime.datetime.combine(x[5], x[6]) for x in data]
+    timedate2 = [datetime.datetime.combine(x[7], x[8]) for x in data]
+    data = [list(x) for x in data]
+
+    for i in range(len(data)):
+        data[i][5] = timedate1[i]
+        data[i][6] = timedate2[i]
+
+    return render_template("calendar1.html", data=data)
+
+@app.route("/addevent", methods=['GET', 'POST'])
+def addevent():
+    eform = EventForm()
+    mycursor.execute("select tag from eventags")
+    value = mycursor.fetchall()
+    value = [x[0] for x in value]
+    if request.method == 'POST':
+
+        if eform.tag.data not in value:
+            x = 0
+            newtag = eventags(t = eform.tag.data, c=x)
+            db.session.add(newtag)
+            db.session.commit()
+
+        newevents = events(t=eform.title.data, d=eform.description.data, v=eform.venue.data, tag=eform.tag.data,
+                           sd=eform.sdate.data,
+                           st=eform.stime.data, ed=eform.edate.data, et=eform.etime.data, un=current_user.mail_id,
+                           c=eform.color.data)
+        db.session.add(newevents)
         db.session.commit()
-        return "submited successfully"
-    return render_template('post.html',form = post_form)
+        return "success"
+    return render_template('create_event.html', form=eform)
 
 @app.route("/logout", methods=['GET'])
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
--- app/models.py
+++ app/models.py
@@ -12,6 +12,49 @@ class Credentials(UserMixin, db.Model):
     mail_id = db.Column(db.String(50), unique = True, nullable = False)
     password = db.Column(db.String(), nullable= False)
 
+    liked = db.relationship(
+        'PostLike',
+        foreign_keys='PostLike.user_id',
+        backref='credentials', lazy='dynamic')
+
+    def like_post(self, post_id):
+        if not self.has_liked_post(post_id):
+            print(self.id)
+            like = PostLike(user_id=self.id, post_id=post_id)
+            db.session.add(like)
+            db.session.commit()
+
+    def unlike_post(self, post_id):
+        if self.has_liked_post(post_id):
+            PostLike.query.filter_by(
+                user_id=self.id,
+                post_id=post_id).delete()
+            db.session.commit()
+
+    def has_liked_post(self, post_id):
+        return PostLike.query.filter(
+            PostLike.user_id == self.id,
+            PostLike.post_id == post_id).count() > 0
+
+    def report_post(self, post_id):
+        if not self.has_reported_post(post_id):
+            print(self.id)
+            report = PostReport(user_id=self.id, post_id=post_id)
+            db.session.add(report)
+            db.session.commit()
+
+    def unreport_post(self, post_id):
+        if self.has_reported_post(post_id):
+            PostReport.query.filter_by(
+                user_id=self.id,
+                post_id=post_id).delete()
+            db.session.commit()
+
+    def has_reported_post(self, post_id):
+        return PostReport.query.filter(
+            PostReport.user_id == self.id,
+            PostReport.post_id == post_id).count() > 0
+
     def get_reset_password_token(self, expires_in=6000):
         return jwt.encode(
             {'reset_password': self.mail_id, 'exp': time() + expires_in},
@@ -27,6 +70,17 @@ class Credentials(UserMixin, db.Model):
         print("heyymodels",id)
         return id
 
+class PostLike(db.Model):
+    __tablename__ = 'post_like'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('credentials.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('postss.id'))
+
+class PostReport(db.Model):
+    __tablename__ = 'post_report'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('credentials.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('postss.id'))
 
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -36,11 +90,84 @@ class Profile(db.Model):
     department = db.Column(db.String(), nullable= False)
     degree = db.Column(db.String(), nullable= False)
 
-class Posts(db.Model):
+class newprofile(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    mail_id = db.Column(db.String(25), nullable = False)
+    mail_id = db.Column(db.String(50), unique = True, nullable = False)
+    full_name = db.Column(db.String(), nullable= False)
+    year = db.Column(db.Integer, nullable = False)
+    department = db.Column(db.String(), nullable= False)
+    degree = db.Column(db.String(), nullable= False)
+
+class Postss(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mail_id = db.Column(db.String(25), unique=True, nullable=False)
     date = db.Column(db.DateTime(timezone=False), nullable=False)
-    post_description =db.Column(db.String(), nullable = False)
-    tag1 = db.Column(db.String(), nullable = False)
-    tag2 = db.Column(db.String())
-    tag3 = db.Column(db.String())
+    post_description = db.Column(db.String(),nullable=False)
+    tag1  = db.Column(db.String(),nullable=False)
+    tag2 = db.Column(db.String(),nullable=True)
+    tag3 = db.Column(db.String(),nullable=True)
+    post_img = db.Column(db.LargeBinary, nullable=False)
+    reports = db.relationship('PostReport',backref='post',lazy = 'dynamic')
+    def __init__(self,mid,d,pd,t1,t2,t3,pi):
+        self.mail_id=mid
+        self.date = d
+        self.post_description =pd
+        self.tag1 =t1
+        self.tag2 = t2
+        self.tag3 =t3
+        self.post_img = pi
+        #print(pd,t1,t2,t3,sep=" ")
+
+class post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mail_id = db.Column(db.String(25), unique=True, nullable=False)
+    date = db.Column(db.DateTime(timezone=False), nullable=False)
+    post_description = db.Column(db.String(), nullable=False)
+    tag1 = db.Column(db.String(), nullable=False)
+    tag2 = db.Column(db.String(), nullable=True)
+    tag3 = db.Column(db.String(), nullable=True)
+    post_img = db.Column(db.LargeBinary, nullable=True)
+
+    def __init__(self, mid, d, pd, t1, t2, t3, pi):
+        self.mail_id = mid
+        self.date = d
+        self.post_description = pd
+        self.tag1 = t1
+        self.tag2 = t2
+        self.tag3 = t3
+        self.post_img = pi
+
+
+class events(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(), nullable=False)
+    title = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    venue = db.Column(db.String(), nullable=False)
+    tag = db.Column(db.String(), nullable=False)
+    sdate = db.Column(db.DateTime(timezone=False),nullable=False)
+    stime = db.Column(db.DateTime(),nullable=False)
+    edate = db.Column(db.DateTime(timezone=False))
+    etime = db.Column(db.DateTime())
+    color = db.Column(db.String(), nullable=False)
+
+    def __init__(self,un,t,d,v,tag,sd,st,ed,et,c):
+        self.username=un
+        self.title=t
+        self.description=d
+        self.venue=v
+        self.tag=tag
+        self.sdate=sd
+        self.stime=st
+        self.edate=ed
+        self.etime=et
+        self.color=c
+
+class eventags(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(), nullable=False)
+    count = db.Column(db.Integer,nullable=False)
+
+    def __init__(self,t,c):
+        self.tag= t
+        self.count= c
